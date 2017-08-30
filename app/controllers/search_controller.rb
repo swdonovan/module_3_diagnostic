7d16b228067d3fe95b1e1d6ca1d50db5ce3b32b7
Wil Donovan <wil.donovan@gmail.com>
2017-08-30 10:02:28 -0600

    moved back to net/http faraday was being incredibly beligerent

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,5 +1,7 @@
 class SearchController < ApplicationController
   def index
+    response = StationData.new(params[:q])
+    stations = response.station_search
     binding.pry
   end
 end
--- app/services/station_data.rb
+++ app/services/station_data.rb
@@ -1,3 +1,5 @@
+require 'net/http'
+
 class StationData
   attr_reader :zipcode,
               :quantity,
@@ -9,7 +11,9 @@ class StationData
   end
 
   def station_search
-    response = Faraday.new(:url =>
-            "/api/alt-fuel-stations/v1/nearest.json?api_key=#{ENV[NREL_KEY]}&location=#{zipcode}&fuel_type=ELEC&LPG&limit=#{quantity}")
+    response = get_request("https://developer.nrel.gov/api/alt-fuel-stations/v1/nearest.json?api_key=#{ENV['NREL_KEY']}&location=#{zipcode}&fuel_type=ELEC%2CLPG&limit=#{quantity}")
+    stations = parse_response(response)
+            binding.pry
+    stations.map { |station| Stations.new(station) }
   end
 end
